f992b472ff9ae01c00a83aeab1e0654f2944a4c3
mac <mac@macs-Mac.local>
2019-02-06 16:46:53 +0200

    removed extra folder

--- make_neat_angular_module.sh
+++ make_neat_angular_module.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo  CONSTRUCTING THE ROUTE STRING 
-echo // {path: \'$1\', loadChildren: \'./modules/$1/$1/$1.module#$2Module\'}, >> src/app/app-routing.module.ts
+echo // {path: \'$1\', loadChildren: \'./modules/$1/$1.module#$2Module\'}, >> src/app/app-routing.module.ts
 echo MAKING THE MODULES...
 ng g m modules/$1 --routing && ng g c modules/$1
 echo CONTRUCTING THE STRING FOR THE ROUTE IN THE MODULE CREATED
-echo // import { $1Component } from \'./$1.component\' >> src/app/modules/$1/$1-routing.module.ts
-echo // import { $1Component } from \'./$1.component\'
-echo // {path: \'\', component: $1Component },
+echo // import { $2Component } from \'./$1.component\' >> src/app/modules/$1/$1-routing.module.ts
+echo // import { $2Component } from \'./$1.component\'
+echo // {path: \'\', component: $2Component },
 echo // {path: \'\', component: $2Component }, >> src/app/modules/$1/$1-routing.module.ts
