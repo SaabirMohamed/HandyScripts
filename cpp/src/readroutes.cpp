d4fe05c13622fffbc03df5aa62936c668e6da588
mac <mac@macs-Mac.local>
2019-02-08 15:16:31 +0200

    some code sorting and fixing typos...not compiled

--- cpp/src/readroutes.cpp
+++ cpp/src/readroutes.cpp
@@ -7,40 +7,44 @@ using namespace std;
 
 int main(int argc, char** argv){
 
-ifstream file (argv[1]); //file just has some sentences
-ifstream outFile (argv[2]); // modified
+ifstream file (argv[1]); // the angular default app routing file
+ifstream outFile (argv[2]); // the angular file to output (a temp file)
 if (!file) {
 cout << "unable to open file";
-// return false;
+// return 0;
 }
 std::ofstream log(argv[2], std::ios_base::app | std::ios_base::out);
 string temp;
 string targeta = argv[4];
 string targetb = argv[5];
-while (getline (file, temp))
-{
-  //this is the string  I look for and append 
-  if(temp == "  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},") {
 
-      log << "  {path: '" + targeta + "', loadChildren: './modules/" + targeta+  "/" + targeta + ".module#"+ targetb + "Module'}\n  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},\n";
-      
-  }  else {
-        log << temp << "\n";
-  }
-  
-  }
-  
-    std::ofstream log(argv[3], std::ios_base::app | std::ios_base::out);
-    string temp2;
-    while (getline(file2, temp2)) {
-            if (temp2 == "import { Routes, RouterModule } from '@angular/router';") {
-              log << "import { Routes, RouterModule } from '@angular/router';\nimport { " + tagetb +"Component } from './"+ targeta + ".component';\n";
-            }
-            else if(temp2 == "const routes: Routes = [") { //in the first case
-            log << "const routes: Routes = [\n{ path: '" + targeta + "', component: "+ targetb + "Component}\n];";
-            }
-            else {
+// LOOPING THROUGH THE DEFAULT app-routing.module.ts file and replacing with the new modules routes
+while (getline (file, temp))
+{       
+        if(temp == "  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},") {
+
+        log << "  {path: '" + targeta + "', loadChildren: './modules/" + targeta+  "/" + targeta + ".module#"+ targetb + "Module'}\n  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},\n";
+        
+        }  else 
+        {
             log << temp << "\n";
-            }
-    }
+        } 
+}
+  
+// LOOPING THROUGH THE CREATED modules default routing file and replacing with the new modules routes
+std::ofstream log(argv[3], std::ios_base::app | std::ios_base::out);
+string temp2;
+while (getline(file2, temp2)) 
+{
+        if (temp2 == "import { Routes, RouterModule } from '@angular/router';") { // the first injection case
+            log << "import { Routes, RouterModule } from '@angular/router';\nimport { " + targetb +"Component } from './"+ targeta + ".component';\n";
+        }
+        else if(temp2 == "const routes: Routes = [") { // the second injection case
+        log << "const routes: Routes = [\n{ path: '" + targeta + "', component: "+ targetb + "Component}\n];";
+        }
+        else {
+        log << temp2 << "\n";
+        }
+}
+
 }
