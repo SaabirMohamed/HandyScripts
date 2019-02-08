d3bcc7030fbf20fbe77b71c87c2a9d71411a4ada
mac <mac@macs-Mac.local>
2019-02-08 15:43:41 +0200

    save before testing compile

--- cpp/src/readroutes.cpp
+++ cpp/src/readroutes.cpp
@@ -6,24 +6,34 @@ using namespace std;
 
 
 int main(int argc, char** argv){
-
-ifstream file (argv[1]); // the angular default app routing file
-ifstream outFile (argv[2]); // the angular file to output (a temp file)
-if (!file) {
-cout << "unable to open file";
-// return 0;
-}
-std::ofstream log(argv[2], std::ios_base::app | std::ios_base::out);
-string temp;
 string targeta = argv[4];
 string targetb = argv[5];
+string ngDefRouteFile = argv[1];
+string ngModFile = argv[2];
 
-// LOOPING THROUGH THE DEFAULT app-routing.module.ts file and replacing with the new modules routes
+ifstream file (argv[1]); // the angular default app routing file
+ifstream file2 (argv[2]); // the angular file to output (a temp file)
+if (!file) {
+cout << "unable to open file";
+ return 0;
+}
+// some declarations params and strings needed
+
+string searchA = "  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},"; //leading space is important neater output
+string replaceA = "  {path: '" + targeta + "', loadChildren: './modules/" + targeta+  "/" + targeta + ".module#"+ targetb + "Module'}\n  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},\n";
+string searchB = "import { Routes, RouterModule } from '@angular/router';";
+string replaceB = "import { Routes, RouterModule } from '@angular/router';\nimport { " + targetb +"Component } from './"+ targeta + ".component';\n";
+string searchC = "const routes: Routes = [";
+string replaceC = "const routes: Routes = [\n{ path: '" + targeta + "', component: "+ targetb + "Component}\n];";
+
+// LOOPING THROUGH THE DEFAULT app-routing.module.ts (file path passed in by user argv[2]) 
+std::ofstream log(argv[2], std::ios_base::app | std::ios_base::out);
+string temp;
 while (getline (file, temp))
 {       
-        if(temp == "  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},") {
+        if(temp == searchA) {
 
-        log << "  {path: '" + targeta + "', loadChildren: './modules/" + targeta+  "/" + targeta + ".module#"+ targetb + "Module'}\n  {path: '**', redirectTo: '', loadChildren: './modules/pnf/pnf.module#PnfModule'},\n";
+        log << replaceA;
         
         }  else 
         {
@@ -36,11 +46,11 @@ std::ofstream log(argv[3], std::ios_base::app | std::ios_base::out);
 string temp2;
 while (getline(file2, temp2)) 
 {
-        if (temp2 == "import { Routes, RouterModule } from '@angular/router';") { // the first injection case
-            log << "import { Routes, RouterModule } from '@angular/router';\nimport { " + targetb +"Component } from './"+ targeta + ".component';\n";
+        if (temp2 == searchB) { // the first injection case
+            log << replaceB;
         }
-        else if(temp2 == "const routes: Routes = [") { // the second injection case
-        log << "const routes: Routes = [\n{ path: '" + targeta + "', component: "+ targetb + "Component}\n];";
+        else if(temp2 == searchC) { // the second injection case
+        log << replaceC;
         }
         else {
         log << temp2 << "\n";
